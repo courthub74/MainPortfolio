c1eae75438e8562aabc9a3790ba8f4aa38c9cfe4
Courdevelops <courdevelops@gmail.com>
2024-05-21 17:49:20 -0400

    experience ready for word transitions

---
+++ experience.js
@@ -0,0 +1,23 @@
+// Query the titles and Logos
+
+let job_1 = document.getElementById('experience_square_one');
+
+let job_2 = document.getElementById('experience_square_two');
+
+let job_3 = document.getElementById('experience_square_three');
+
+
+// Press the Title Function
+job_1.addEventListener('pointerdown', () => {
+    console.log('Job One Clicked');
+});
+
+job_2.addEventListener('pointerdown', () => {
+    console.log('Job Two Clicked');
+});
+
+job_3.addEventListener('pointerdown', () => {
+    console.log('Job Three Clicked');
+});
+
+
