a8a78c0f7196f3020b2f8cf280c421b3fd0abdbc
Courdevelops <courdevelops@gmail.com>
2024-05-25 17:28:28 -0400

    logos on skills sized

--- skills.js
+++ skills.js
@@ -1,16 +1,20 @@
-// Query the Circle
-let circle_one = document.getElementById('skill_circle_one');
+// Query the outer Circle
+
+// Query the inner Circle
+let html_skill_circle = document.getElementById('html_skill');
 let python_skill_circle = document.getElementById('python_skill');
 
 // Query the Line
-// let line_one = document.getElementById('line_one');
+let line_html = document.getElementById('line_html');
 let line_python = document.getElementById('line_python');
 
 // Query the Logo
 let python_skill = document.getElementById('python');
+let html_skill = document.getElementById('html');
 
 // Query the Description
 let python_description = document.getElementById('python_description');
+let html_description = document.getElementById('html_description');
 
 // Mobile
 
@@ -20,6 +24,13 @@ let python_description = document.getElementById('python_description');
 let widthSkills = window.matchMedia("(min-width: 1180px)");
 
 if(widthSkills.matches) {
+
+    // ALL OF THE CIRCLES
+    // HTML
+    html_skill_circle.addEventListener('mouseover', () => {
+        
+    })
+    // PYTHON
     // add Event listener to the circle
     python_skill_circle.addEventListener('mouseover', () => {
         python_skill_circle.classList.add('hover');
