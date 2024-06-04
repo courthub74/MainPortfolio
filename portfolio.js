69d4fdf83bc78db61d92adc59f1defa9d198a9b5
Courdevelops <courdevelops@gmail.com>
2024-06-04 18:11:21 -0400

    ready to enter portfolio content

--- portfolio.js
+++ portfolio.js
@@ -8,12 +8,23 @@ let port_logo_one = document.getElementById('workflow_app');
 let port_content_one = document.getElementById('workflow_app_content');
 
 // Query the Close button
-
+let port_close_one = document.getElementById('port_close_one');
 
 // MOBILE
+
+// Square One
+
+// Logo to click it on
 port_logo_one.addEventListener('pointerdown', () => {
     console.log("Box One Pressed");
-    port_sq_one.classList.toggle('tapped');
-    port_logo_one.classList.toggle('tapped');
-    port_content_one.classList.toggle('tapped');
+    port_sq_one.classList.add('tapped');
+    port_logo_one.classList.add('tapped');
+    port_content_one.classList.add('tapped');
 });
+
+// Close button to click off
+port_close_one.addEventListener('pointerdown', () => {
+    port_sq_one.classList.remove('tapped');
+    port_logo_one.classList.remove('tapped');
+    port_content_one.classList.remove('tapped');
+})
