c773e6f0cbb32bfbb110b705e3860599ef83a5ea
Courdevelops <courdevelops@gmail.com>
2024-06-07 14:26:04 -0400

    workflow app mobile done

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,7 @@
 // Query the Whole Squares (to light up the top border)
+let port_sq_one = document.getElementById('portfolio_square_one');
+let port_sq_two = document.getElementById('portfolio_square_two');
+let port_sq_three = document.getElementById('portfolio_square_three');
 
 // Query the Logos (for pressing the button)
 let port_logo_one = document.getElementById('workflow_app');
@@ -6,12 +9,15 @@ let port_logo_two = document.getElementById('create_collab');
 let port_logo_three = document.getElementById('luminex_solutions');
 
 // Query the Elements in the Square (for positioning)
-let port_elements_one = document.getElementById('portfolio_one');
+// let port_elements_one = document.getElementById('portfolio_one');
 
 // Query the Content (what pops up when dropped down)
+let port_content_one = document.getElementById('workflow_app_content');
+let port_content_two = document.getElementById('create_collab_content');
 
 // Query the Close button (to close the square)
-
+let port_close_one = document.getElementById('port_close_one');
+let port_close_two = document.getElementById('port_close_two');
 
 // MOBILE
 
@@ -19,6 +25,16 @@ let port_elements_one = document.getElementById('portfolio_one');
 
 port_logo_one.addEventListener('pointerdown', () => {
     console.log("Box One Pressed");
+    port_sq_one.classList.add('tapped');
+    port_logo_one.classList.add('tapped');
+    port_content_one.classList.add('tapped');
+});
+
+// Close button to click off
+port_close_one.addEventListener('pointerdown', () => {
+    port_sq_one.classList.remove('tapped');
+    port_logo_one.classList.remove('tapped');
+    port_content_one.classList.remove('tapped');
 });
 
 // SQUARE TWO
