4dde4d3b6a4b214835f42cfb8440873af11ae40f
Courdevelops <courdevelops@gmail.com>
2024-06-07 14:17:44 -0400

    z indexes on the mobile

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,16 @@
-// Query the Whole Squares
+// Query the Whole Squares (to light up the top border)
 
-// Query the Logos
+// Query the Logos (for pressing the button)
 let port_logo_one = document.getElementById('workflow_app');
 let port_logo_two = document.getElementById('create_collab');
 let port_logo_three = document.getElementById('luminex_solutions');
 
-// Query the Content
+// Query the Elements in the Square (for positioning)
+let port_elements_one = document.getElementById('portfolio_one');
 
-// Query the Close button
+// Query the Content (what pops up when dropped down)
+
+// Query the Close button (to close the square)
 
 
 // MOBILE
